f4c42c15a408199f0b46f0c90e1cfce95f2ab587
Shai Ayal <shaiay@gmail.com>
2013-09-21 17:55:10 +0300

    trivial changes for xkcd

--- src/org/ayal/xkcd2mobile/XKCDUrlActivity.java
+++ src/org/ayal/xkcd2mobile/XKCDUrlActivity.java
@@ -1,16 +1,16 @@
-package com.rileybrewer.yturlfix;
+package org.ayal.xkcd2mobile;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
-public class YTUrlActivity extends Activity {
-    private static final String YOUTUBE = "http://www.youtube.com/watch?v=%s";
+public class XKCDUrlActivity extends Activity {
+    private static final String XKCD_M = "http://m.xkcd.org/%s";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String uriString = String.format(YOUTUBE, getIntent().getData().getLastPathSegment());
+        String uriString = String.format(XKCD_M, getIntent().getData().getLastPathSegment());
         Intent intent = new Intent();
         intent.setData(Uri.parse(uriString));
         startActivity(intent);
